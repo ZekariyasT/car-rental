9fceb018d9c1852bf935f5f1b51e07b222a3955f
Zekariyas Tesfaye <99519232+ZekariyasT@users.noreply.github.com>
2025-02-23 19:43:03 +0300

    creating routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
-import logo from "./logo.svg";
 import "./App.css";
-
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import BookingCar from "./pages/BookingCar";
 function App() {
   return (
     <div className="App">
-      <h1>Car Rental</h1>
+      <BrowserRouter>
+        <Route path="/" exact component={Home} />
+        <Route path="/login" exact component={Login} />
+        <Route path="/register" exact component={Register} />
+        <Route path="/bookingcar" exact component={BookingCar} />
+      </BrowserRouter>
     </div>
   );
 }
